f3dacb68944cd7f7dec18e946e5d3dbd9269a89f
Christian Lohr <christian.lohr@gmail.com>
2017-11-30 21:44:17 -0500

    Updated intro code

--- src/components/Intro/Intro.vue
+++ src/components/Intro/Intro.vue
@@ -1,12 +1,11 @@
 <template>
-    <intro-view :salutation="title" :name="name" :message="msg"></intro-view>
+    <intro-view :salutation="title" :message="msg"></intro-view>
 </template>
 
 <script>
     export default {
         data() {
             return {
-                name: 'Christian',
                 title: 'Welcome to the VueJS Demo',
                 msg: 'This application is designed to showcase some of the finer features of VueJS + Web Components.'
             }
